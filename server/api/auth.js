d91fc55e26bee4db6455cc03299bd8960265689f
Alicia Hetrick <aliciahetrick@gmail.com>
2023-03-19 16:07:40 -0400

    Add auth routes

---
+++ server/api/auth.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const User = require("./models/User");
+module.exports = router;
+
+router.post("/login", async (req, res, next) => {
+  try {
+    res.send({ token: await User.authenticate(req.body) });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/signup", async (req, res, next) => {
+  try {
+    const user = await User.create(req.body);
+    res.send({ token: await user.generateToken() });
+  } catch (err) {
+    if (err) {
+      // res.status(401).send('User already exists')
+      console.log(err);
+    } else {
+      next(err);
+    }
+  }
+});
+
+router.get("/me", async (req, res, next) => {
+  try {
+    res.send(await User.findByToken(req.headers.authorization));
+  } catch (ex) {
+    next(ex);
+  }
+});
