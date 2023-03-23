0cc267203f0bae602dca115730641cc99289c89b
Alicia Hetrick <aliciahetrick@gmail.com>
2023-03-22 20:49:07 -0400

    Add type for button click event

--- src/features/pages/EditAccount.tsx
+++ src/features/pages/EditAccount.tsx
@@ -2,7 +2,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../store";
 import { updateUser } from "../../store/usersSlice";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, MouseEvent, useState } from "react";
 import { logoutUser } from "../../store/authSlice";
 import { useNavigate } from "react-router-dom";
 
@@ -16,7 +16,6 @@ const EditAccount = () => {
   const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
   const loggedInUser = useSelector((state: RootState) => state.auth);
-  // console.log("logged in user", loggedInUser);
 
   const [toggleForm, setToggleForm] = useState("");
   const [formValues, setFormValues] = useState("");
@@ -26,16 +25,17 @@ const EditAccount = () => {
     console.log(formValues);
   };
 
-  const editInfo = async (e) => {
-    if (e.target.value === "username") {
+  const editInfo = async (e: MouseEvent<HTMLElement>) => {
+    const target = e.target as HTMLButtonElement;
+    if (target.value === "username") {
       setToggleForm("username");
       setFormValues("");
-    } else if (e.target.value === "password") {
+    } else if (target.value === "password") {
       setToggleForm("password");
       setFormValues("");
     } else {
       const updateObj: updateObj = { _id: loggedInUser._id };
-      if (e.target.value.includes("Username")) {
+      if (target.value.includes("Username")) {
         updateObj.username = formValues;
       } else {
         updateObj.password = formValues;
