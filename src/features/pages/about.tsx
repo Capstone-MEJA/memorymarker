8e3f7b93700a75fad3006221cea0418e8601c3bb
Erica Young <eyoung135@gmail.com>
2023-03-23 12:53:18 -0400

    finished about page

--- src/features/pages/about.tsx
+++ src/features/pages/about.tsx
@@ -1,41 +1,59 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const AboutText = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 20px;
-    height: 70vh;
-`
-const AboutTitle = styled.div`
-display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 50px;
-    height: 70vh;
-`
-const AboutSection = styled.div``
-const Paragraph = styled.div``
-
 
 const About: React.FC = () => {
     return (
         <AboutSection>
             <AboutTitle>
-            About Us
+            About
             </AboutTitle>
-
             <AboutText>
-                Welcome to the world's most popular social media platform. 
+                Welcome to the world's most popular social media platform. Our mission is to help you tell your story.
             </AboutText>
             <Paragraph>
                 Mark the Memories you Make
             </Paragraph>
-
+            <Paragraph>
+                We are a story-driven platform.  We all have memories we don't want to forget. Our page is a place where people and experiences come together.  Whether it be where you had your first kiss or where you had your latest meal, mark it on the map! This is a site that fits you and your journey on this earth along with all 7.5+ billion of us. 
+            </Paragraph>
+            <Paragraph>
+                Get Started: Marking your Memories or Make your Mark? 
+            </Paragraph>
+            <Paragraph>
+                Start documenting your journey by creating your account! Have a night you don't want to forget? Mark it on the map! Create a post and journal your thoughts.  It could be as long as you want or as short.  Where was it? Who were you with? How did you feel? Share your voice, express yourself. 
+            </Paragraph>
+            <Paragraph>
+                About: The Memory Makers            
+            </Paragraph>
+            <Paragraph>
+                Founded in 2023 by a team of makers, thinkers, explorers.  We wanted a place for people to gather, share their journies and see other people's journies all over the map.  Making this big world feel a little smaller.  Everyone has memories and they grow as we all grow together. We're committed to helping you tell your story.
+            </Paragraph>
         </AboutSection>
-        
     )
 }
 
-export default About
+export default About;
+
+const AboutText = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 30px;
+    height: 50vh;
+`
+const AboutTitle = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 50px;
+    height: 50vh;
+`
+const AboutSection = styled.div``
+const Paragraph = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 30px;
+    height: 50vh;
+`
