278b9a4f74d378ebb4e45127b0e408739d4ec94d
mandy-chin <chin.mandyy@gmail.com>
2023-03-26 23:24:50 -0400

    restrict access to certain pages if not logged in

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -1,4 +1,6 @@
-import {  Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
 import Map from "../features/map/Map";
 import UserProfile from "../features/pages/UserProfile";
 import EditAccount from "../features/pages/EditAccount";
@@ -8,16 +10,23 @@ import Login from "../features/auth/Login";
 import ManagePosts from "../features/pages/ManagePosts";
 
 const AppRoutes = () => {
+  const auth = useSelector((state: RootState) => state.auth);
   return (
     <div>
       <Routes>
+        {auth._id ? (
+          <>
+            <Route path="/account" element={<UserProfile />} />
+            <Route path="/information" element={<EditAccount />} />
+            <Route path="/myposts" element={<ManagePosts />} />
+          </>
+        ) : (
+          ""
+        )}
         <Route path="/home" element={<Map />} />
-        <Route path="/account" element={<UserProfile />} />
-        <Route path="/information" element={<EditAccount />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/myposts" element={<ManagePosts />} />
-        <Route path='/about' element={<About />} />
+        <Route path="/about" element={<About />} />
         <Route path="*" element={<Map />} />
       </Routes>
     </div>
