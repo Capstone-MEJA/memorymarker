6dd56e16034768d65b80538953a713c8f7382f27
Alicia Hetrick <aliciahetrick@gmail.com>
2023-03-28 12:49:42 -0400

    Revert port

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
-// const PORT = process.env.PORT || 8080;
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const app = require("./app");
 const mongoose = require("mongoose");
 const password = process.env.MONGODB_ATLAS_PASSWORD;
