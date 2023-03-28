9994078ce8618fafacadaa11e874e49945ca51aa
Alicia Hetrick <aliciahetrick@gmail.com>
2023-03-28 12:16:14 -0400

    Update webpack to use Dotenv

--- webpack.common.js
+++ webpack.common.js
@@ -1,50 +1,50 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 // const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
-const Dotenv = require('dotenv-webpack');
+const Dotenv = require("dotenv-webpack");
 // const fs = require('fs');
 // const os = require('os');
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: "./src/index.tsx",
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
+      template: "src/index.html",
     }),
     new CopyPlugin({
-      patterns: [{ from: 'src/icons' }],
+      patterns: [{ from: "src/icons" }],
     }),
     // new NodePolyfillPlugin(),
-    new Dotenv()
+    new Dotenv({ systemvars: true }),
   ],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
     clean: true,
   },
 };
