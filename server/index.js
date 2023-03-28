143337db005a0d49958a07077fcdf9da08dd2c01
Alicia Hetrick <aliciahetrick@gmail.com>
2023-03-28 11:43:11 -0400

    Add mongodb atlas connection string

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const PORT = process.env.PORT || 8080;
 const app = require("./app");
 const mongoose = require("mongoose");
+const password = process.env.MONGODB_ATLAS_PASSWORD;
+
+const uri = `mongodb+srv://makers:${password}@memory-marker.sdm16jf.mongodb.net/?retryWrites=true&w=majority`;
 
 const init = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/memorymarker");
+    await mongoose.connect(uri);
     // start listening (and create a 'server' object representing our server)
     app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
   } catch (ex) {
