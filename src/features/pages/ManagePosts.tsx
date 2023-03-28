0e030ec0f39dc8e45a06d14ce62e7b9fc56a4fbb
mandy-chin <chin.mandyy@gmail.com>
2023-03-28 15:45:55 -0400

    testing mobile version

--- src/features/pages/ManagePosts.tsx
+++ src/features/pages/ManagePosts.tsx
@@ -9,6 +9,7 @@ import {
 } from "../../store/postsSlice";
 import { IsPost } from "../../interface";
 import styled from "styled-components";
+import { device } from "../../styles/global";
 
 const ManagePosts = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -53,110 +54,164 @@ const ManagePosts = () => {
   }
 
   return (
-    <section>
-      {posts.filter((individual) => individual.user._id === loggedInUser._id)
-        .length === 0 ? (
-        "No posts yet. Go out and make some new memories! :)"
-      ) : (
-        <Table>
-          <thead>
-            <Tr>
-              <Th>Title</Th>
-              <Th>Description</Th>
-              <Th>Tags</Th>
-              <Th>Date</Th>
-            </Tr>
-          </thead>
-          <tbody>
-            {posts.length < 0
-              ? ""
-              : posts
-                  .filter(
-                    (individual) => individual.user._id === loggedInUser._id
-                  )
-                  .map((post: IsPost) => (
-                    <Tr key={post._id}>
-                      <Td>
-                        {editMode && formValue._id === post._id ? (
-                          <input
-                            onChange={changeHandler}
-                            name="title"
-                            value={formValue.title}
-                          />
-                        ) : (
-                          post.title
-                        )}
-                      </Td>
-                      <Td>
-                        {editMode && formValue._id === post._id ? (
-                          <input
-                            onChange={changeHandler}
-                            name="description"
-                            value={formValue.description}
-                          />
-                        ) : (
-                          post.description
-                        )}
-                      </Td>
-                      <Td>
-                        {editMode && formValue._id === post._id ? (
-                          <input
-                            onChange={changeHandler}
-                            name="tags"
-                            value={formValue.tags}
-                          />
-                        ) : (
-                          post.tags
-                        )}
-                      </Td>
+    <Wrapper>
+      <LogoWrapper>
+        <Logo src="logo.png" />
+      </LogoWrapper>
+      <Title>Your Posts</Title>
+      <section>
+        {posts.filter((individual) => individual.user._id === loggedInUser._id)
+          .length === 0 ? (
+          "No posts yet. Go out and make some new memories! :)"
+        ) : (
+          <Table>
+            <Thead>
+              <Tr>
+                <Th>Title</Th>
+                <Th>Description</Th>
+                {/* <Th>Tags</Th> */}
+                <Th>Date</Th>
+                <Th>Edit Post</Th>
+                <Th>Delete Post</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {posts.length < 0
+                ? ""
+                : posts
+                    .filter(
+                      (individual) => individual.user._id === loggedInUser._id
+                    )
+                    .map((post: IsPost) => (
+                      <Tr key={post._id}>
+                        <Td>
+                          {editMode && formValue._id === post._id ? (
+                            <input
+                              onChange={changeHandler}
+                              name="title"
+                              value={formValue.title}
+                            />
+                          ) : (
+                            post.title
+                          )}
+                        </Td>
+                        <Td>
+                          {editMode && formValue._id === post._id ? (
+                            <input
+                              onChange={changeHandler}
+                              name="description"
+                              value={formValue.description}
+                            />
+                          ) : (
+                            post.description
+                          )}
+                        </Td>
+                        {/* <Td>
+                          {editMode && formValue._id === post._id ? (
+                            <input
+                              onChange={changeHandler}
+                              name="tags"
+                              value={formValue.tags}
+                            />
+                          ) : (
+                            post.tags
+                          )}
+                        </Td> */}
 
-                      <Td>{post.timeStamp.slice(0, -32)}</Td>
-                      <Td>
-                        <button
-                          className="btn primary-btn"
-                          onClick={(event) => {
-                            onClickHandler({
-                              method: (event.target as HTMLInputElement).value,
-                              _id: post._id,
-                              title: post.title,
-                              description: post.description,
-                              tags: post.tags,
-                            });
-                          }}
-                          value={!editMode ? "Edit" : "Save Changes"}
-                        >
-                          {!editMode ? "Edit" : "Save Changes"}
-                        </button>
-                      </Td>
-                      <Td>
-                        <button
-                          className="btn delete-btn"
-                          onClick={() => {
-                            handleDelete(post._id);
-                          }}
-                        >
-                          Delete
-                        </button>
-                      </Td>
-                    </Tr>
-                  ))}
-          </tbody>
-        </Table>
-      )}
-    </section>
+                        <Td>{post.timeStamp.slice(0, -32)}</Td>
+                        <Td>
+                          <button
+                            className="btn primary-btn"
+                            onClick={(event) => {
+                              onClickHandler({
+                                method: (event.target as HTMLInputElement)
+                                  .value,
+                                _id: post._id,
+                                title: post.title,
+                                description: post.description,
+                                // tags: post.tags,
+                              });
+                            }}
+                            value={!editMode ? "Edit" : "Save Changes"}
+                          >
+                            {!editMode ? "Edit" : "Save Changes"}
+                          </button>
+                        </Td>
+                        <Td>
+                          <button
+                            className="btn delete-btn"
+                            onClick={() => {
+                              handleDelete(post._id);
+                            }}
+                          >
+                            Delete
+                          </button>
+                        </Td>
+                      </Tr>
+                    ))}
+            </Tbody>
+          </Table>
+        )}
+      </section>
+    </Wrapper>
   );
 };
 
 export default ManagePosts;
 
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 1rem;
+`;
+const LogoWrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  margin: 0px;
+`;
+
+const Logo = styled.img`
+  width: 10rem;
+
+  @media ${device.laptop} {
+    width: 18rem;
+    height: 18rem;
+  }
+`;
+
+const Title = styled.p`
+  font-family: "Playfair Display", serif;
+  font-size: 4rem;
+  text-align: center;
+  margin-bottom: 1rem;
+
+  @media only screen and ${device.mobileLMax}{
+    font-size: 3.5rem;
+  }
+`;
+
 const Table = styled.table`
   width: 100%;
   border-collapse: collapse;
+
+  @media only screen and ${device.mobileLMax}{
+    display: block;
+  }
 `;
 
 const Tr = styled.tr`
-  $:nth-of-type(odd) {
+  :nth-of-type(odd) {
     background: #eee;
+  }
+  @media only screen and ${device.mobileLMax}{
+    display: block;
+
+    // position: absolute;
+    // top: -9999px;
+    // left: -9999px;
+
+    border: 1px solid #ccc;
   }
 `;
 
@@ -168,10 +223,67 @@ const Th = styled.th`
   padding: 6px;
   border: 1px solid #ccc;
   text-align: left;
+
+  @media only screen and ${device.mobileLMax}{
+    display: block;
+
+    position: absolute;
+    top: -9999px;
+    left: -9999px;
+  }
 `;
 
 const Td = styled.td`
   padding: 6px;
   border: 1px solid #ccc;
   text-align: left;
+
+  @media only screen and ${device.mobileLMax}{
+    display: block;
+
+    border: none;
+    border-bottom: 1px solid #eee;
+    position: relative;
+    padding-left: 50%;
+
+    :before {
+      position: absolute;
+      top: 6px;
+      left: 6px;
+      width: 45%;
+      padding-right: 10px;
+      white-space: nowrap;
+    }
+
+    :nth-of-type(1):before {
+      content: "Title";
+    }
+    :nth-of-type(2):before {
+      content: "Description";
+    }
+    // :nth-of-type(3):before {
+    //   content: "Tags";
+    // }
+    :nth-of-type(3):before {
+      content: "Date";
+    }
+    :nth-of-type(4):before {
+      content: "Edit Post";
+    }
+    :nth-of-type(5):before {
+      content: "Delete Post";
+    }
+  }
+`;
+
+const Thead = styled.thead`
+  @media only screen and ${device.mobileLMax}{
+    display: block;
+  }
+`;
+
+const Tbody = styled.tbody`
+  @media only screen and ${device.mobileLMax}{
+    display: block;
+  }
 `;
