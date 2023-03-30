9ddd36d746027651186589744ecff0f8aa151164
Alicia Hetrick <aliciahetrick@gmail.com>
2023-03-30 12:02:07 -0400

    Add comments to /server/index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
+// main entry point for backend server
+
 const PORT = process.env.PORT || 8080;
 const app = require("./app");
 const mongoose = require("mongoose");
 
+/**
+ * Intializes connection with MongoDB database
+ * On main branch, connects via local MongoDB called memorymarker
+ * On deployed branch, connects via MongoDB Atlas called memory-marker
+ */
+
 const init = async () => {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/memorymarker");
-    // start listening (and create a 'server' object representing our server)
     app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
   } catch (ex) {
     console.log(ex);
