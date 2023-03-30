835ee11bea52fa17be0e86e86d5b8eaeb7a3bdd8
Alicia Hetrick <aliciahetrick@gmail.com>
2023-03-30 12:36:41 -0400

    Add comments to /server/api/maps.js

--- server/api/maps.js
+++ server/api/maps.js
@@ -1,8 +1,15 @@
-const router = require('express').Router();
-const { useLoadScript} = require("@react-google-maps/api");
-module.exports = router;
+const router = require("express").Router();
 
+/**
+ * Route serving google map to /api/maps
+ * @name get/map
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ * @return {string} google maps API key
+ */
 
 router.get("/", (req, res, next) => {
-    res.send(process.env.REACT_APP_GOOGLE_MAPS_API_KEY)   
-})
+  res.send(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
+});
+
+module.exports = router;
