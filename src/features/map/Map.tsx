da3ad2a0e330fcda0ec2170230ef9c5fc4fbdc5a
Alicia Hetrick <aliciahetrick@gmail.com>
2023-03-31 08:42:36 -0400

    Add JSdoc comment for Map component

--- src/features/map/Map.tsx
+++ src/features/map/Map.tsx
@@ -1,7 +1,11 @@
 import { useLoadScript } from "@react-google-maps/api";
 import MemoryMap from "./MemoryMap";
 
-// initialize map using API Key
+/**
+ * Component for loading fetching map by api key
+ * @returns A map, loading message, or an error
+ */
+
 const Map = (): JSX.Element => {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY!,
