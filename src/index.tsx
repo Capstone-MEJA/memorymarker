45ef5237e1976f0a872c077a954dd90d54fd6143
Alicia Hetrick <aliciahetrick@gmail.com>
2023-03-30 20:55:09 -0400

    Cleanup /src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,22 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store/index";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./app/App";
 import { GlobalStyle } from "./styles/global";
-// import React from "react";
-
-import "../src/app.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <Router>
     <Provider store={store}>
-    {/* <React.StrictMode> */}
-      <GlobalStyle />
-      <App />      
-    {/* </React.StrictMode> */}
+      <React.StrictMode>
+        <GlobalStyle />
+        <App />
+      </React.StrictMode>
     </Provider>
   </Router>
 );
